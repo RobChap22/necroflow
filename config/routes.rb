7f0ac605d9ff04f06d1f21c13c0d87d16d7890e3
Rob Chapman <autosexual@gmail.com>
2020-09-08 13:45:04 +0900

    null false removed

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'gangs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :gangs do
-      resources :turves, only: [:new, :create]
-    end
+
+  resources :gangs do
+    resources :turves, only: [:new, :create]
   end
 end
--- db/migrate/20200904084430_add_user_to_gang.rb
+++ db/migrate/20200904084430_add_user_to_gang.rb
@@ -1,5 +1,5 @@
 class AddUserToGang < ActiveRecord::Migration[6.0]
   def change
-    add_reference :gangs, :user, null: false, foreign_key: true
+    add_reference :gangs, :user, foreign_key: true
   end
 end
